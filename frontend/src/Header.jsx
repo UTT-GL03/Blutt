66b0c9760393d070a6d7ee1802aa3bc0f78dd5c8
Raphaël Weis <raphael.weis@outlook.com>
2024-10-29 15:25:30 +0100

    feat(App): Added a header component and configured the website logo + favicon.

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="header-title">Blutt</div>
+      <img className="logo" src="/src/assets/logo.png"/>
     </div>
   );
 }
