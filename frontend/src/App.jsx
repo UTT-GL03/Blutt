7cfe7b5a856c8f955cd532a526a3a458b9bc4d87
medchouai <medchouai2@gmail.com>
2024-10-29 17:48:26 +0100

    feat(frontend): Added homepage form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,18 @@ export default function App() {
   return (
     <>
       <Header />
+      <div className="accueil_client">
+      <h2>Selectionnez votre trajet:</h2>
+      <form className="form_client">
+      <label for="destination1">De:</label> 
+      <input type="text" name="destination1" id="destination1" /> 
+      <label for="destination2">A:</label> 
+      <input type="text" name="destination2" id="destination2" /> 
+      <label for="destination2">Date:</label> 
+      <input type="date" id="date" name="date"/><br></br>
+    </form>
+    <button type="submit">Search</button><br></br>
+    </div>
     </>
   );
 }
