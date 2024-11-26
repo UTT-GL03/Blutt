fe6c24f67fdcba51a6eeee3cb17e106695e7e6b8
Raphaël Weis <raphael.weis@outlook.com>
2024-11-26 17:47:25 +0100

    feat: Added pagination for the carpool list.

--- frontend/src/routes/CarpoolSearch.jsx
+++ frontend/src/routes/CarpoolSearch.jsx
@@ -4,16 +4,31 @@ import "../styles/CarpoolSearch.css";
 import CarpoolEntry from "../components/CarpoolEntry.jsx";
 import {useEffect, useState} from "react";
 
+const LIMIT = 10;
+
 export default function CarpoolSearch() {
   const [carpools, setCarpools] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxItems, setMaxItems] = useState();
+
+  const previousPage = () => {
+    if (currentPage !== 0) setCurrentPage(currentPage - 1);
+  }
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  }
 
   useEffect(() => {
-    fetch(`http://localhost:5984/blutt/_all_docs?include_docs=true`)
+    console.log("HELLO");
+    fetch(`http://localhost:5984/blutt/_all_docs?include_docs=true&limit=${LIMIT}&skip=${(currentPage - 1) * LIMIT}`)
       .then(res => res.json())
       .then(data => {
-        setCarpools(data.rows)
+        setCarpools(data.rows);
+        setMaxItems(data.total_rows);
+        console.log(data.rows);
       })
-  }, []);
+  }, [currentPage]);
 
   return (
     <div className="container">
@@ -27,6 +42,13 @@ export default function CarpoolSearch() {
         {carpools.map((carpool) => (
           <CarpoolEntry key={carpool.id} carpool={carpool.doc}/>
         ))}
+      </div>
+      <div className="page-selector">
+        <button type="button" disabled={currentPage === 1} className="button" onClick={previousPage}>Précédent</button>
+        <a onClick={nextPage}
+           className={"current-page"}>{currentPage}</a>
+        <button disabled={currentPage * LIMIT >= maxItems} type="button" className="button" onClick={nextPage}>Suivant
+        </button>
       </div>
     </div>
   );
