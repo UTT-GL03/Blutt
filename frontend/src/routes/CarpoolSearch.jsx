141c92011e63d1f175e16c1e9b2c2085b07a6aaa
Raphaël Weis <raphael.weis@outlook.com>
2024-12-10 14:48:24 +0100

    feat: Style updates in Carpool Search.

--- frontend/src/routes/CarpoolSearch.jsx
+++ frontend/src/routes/CarpoolSearch.jsx
@@ -1,8 +1,8 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Header from "../components/Header.jsx";
 import "../styles/CarpoolSearch.css";
 import CarpoolEntry from "../components/CarpoolEntry.jsx";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 const LIMIT = 10;
 
@@ -13,41 +13,52 @@ export default function CarpoolSearch() {
 
   const previousPage = () => {
     if (currentPage !== 0) setCurrentPage(currentPage - 1);
-  }
+  };
 
   const nextPage = () => {
     setCurrentPage(currentPage + 1);
-  }
+  };
 
   useEffect(() => {
     console.log("HELLO");
-    fetch(`http://localhost:5984/blutt/_all_docs?include_docs=true&limit=${LIMIT}&skip=${(currentPage - 1) * LIMIT}`)
-      .then(res => res.json())
-      .then(data => {
+    fetch(
+      `http://localhost:5984/blutt/_all_docs?include_docs=true&limit=${LIMIT}&skip=${(currentPage - 1) * LIMIT}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
         setCarpools(data.rows);
         setMaxItems(data.total_rows);
-        console.log(data.rows);
-      })
+        window.scrollTo(0, 0);
+      });
   }, [currentPage]);
 
   return (
     <div className="container">
-      <Header/>
-      <Link to="/">
-        <button type="button" className="button">
-          Retour
-        </button>
-      </Link>
+      <Header />
       <div className="carpool-container">
         {carpools.map((carpool) => (
-          <CarpoolEntry key={carpool.id} carpool={carpool.doc}/>
+          <CarpoolEntry key={carpool.id} carpool={carpool.doc} />
         ))}
       </div>
       <div className="page-selector">
-        <button type="button" disabled={currentPage === 1} className="button" onClick={previousPage}>Précédent</button>
-        <a onClick={nextPage}
-           className={"current-page"}>{currentPage}</a>
-        <button disabled={currentPage * LIMIT >= maxItems} type="button" className="button" onClick={nextPage}>Suivant
+        <button
+          type="button"
+          disabled={currentPage === 1}
+          className="button"
+          onClick={previousPage}
+        >
+          Précédent
+        </button>
+        <a onClick={nextPage} className={"current-page"}>
+          {currentPage}
+        </a>
+        <button
+          disabled={currentPage * LIMIT >= maxItems}
+          type="button"
+          className="button"
+          onClick={nextPage}
+        >
+          Suivant
         </button>
       </div>
     </div>
