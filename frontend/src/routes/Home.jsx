bba076f3e3be09ed06908ab0c08ea96142c15741
Raphaël Weis <raphael.weis@outlook.com>
2024-12-10 14:38:21 +0100

    feat: Style fixes in home screen.

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
+import "../styles/Global.css";
 import Header from "../components/Header.jsx";
 
 export default function Home() {
@@ -7,26 +8,33 @@ export default function Home() {
     <>
       <Header />
       <div className="home-container">
-        <h2>Selectionnez votre trajet:</h2>
-        <form className="form_client">
-          <label className="label" htmlFor="destination1">De:</label>
-          <input type="text" name="destination1" id="destination1" />
-          <label htmlFor="destination2">A:</label>
-          <input type="text" name="destination2" id="destination2" />
-          <label htmlFor="destination2">Date:</label>
-          <input type="date" id="date" name="date" />
-          <br></br>
+        <h2>Sélectionnez votre trajet:</h2>
+        <form className="form-client">
+          <div className="input-container">
+            <label htmlFor="destination1">De:</label>
+            <input type="text" name="destination1" id="destination1" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="destination2">A:</label>
+            <input type="text" name="destination2" id="destination2" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="destination2">Date:</label>
+            <input type="date" id="date" name="date" />
+          </div>
         </form>
         <Link to="carpool-search">
-          <button className="button" type="submit">Search</button>
+          <button className="button" type="submit">
+            Search
+          </button>
           <br></br>
         </Link>
       </div>
       <Link to="carpooler-home">
-      <button type="button" className="button">
-        Switch profile
-      </button>
-      <br></br>
+        <button type="button" className="button">
+          Switch profile
+        </button>
+        <br></br>
       </Link>
     </>
   );
